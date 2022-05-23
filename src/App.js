a53f39e239c709ab06cc27ffb6f6f7e225e13911
bpn1604 <bipin.kumar2015@vitalum.ac.in>
2022-05-23 16:27:38 +0530

    e1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from "react";
-
+import AddTask from "./components/AddTask/AddTask";
+import Data from './components/TaskApp'
 function App() {
-  return <div>{/* Code Here */}</div>;
+
+  return <div>{/* Code Here */}
+    <>
+    
+    <Data />
+    <AddTask />
+    </>
+    
+  </div>;
 }
 
 export default App;
